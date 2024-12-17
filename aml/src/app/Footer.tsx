9dacd8a324dbe3b341ee53eff456d9767e53f12d
RossJennison <c0024074@hallam.shu.ac.uk>
2024-12-17 20:08:01 +0000

    Update Footer.tsx

--- aml/src/app/Footer.tsx
+++ aml/src/app/Footer.tsx
@@ -1,29 +1,3 @@
-import './Footer.css';
-
-export async function Footer (){
-    return (
-      <div>
-      <footer>
-        <p>&copy; {new Date().getFullYear()} Advanced Media Library</p>
-        <ul id="leftList">
-          <li>About</li>
-          <li><a href="#">Copyrights</a></li>
-          <li><a href="#">Contact us</a></li>
-          <li><a href="#">ghsdfdfsg</a></li>g
-          </ul>    
-          <ul>
-            <li><a href="#">dfghjfmd</a></li>
-            <li><a href="#">dfhafgb</a></li>
-            <li><a href="#">khsdds</a></li>
-            <li><a href="#">dhkgds</a></li>
-        </ul>
-        <ul id="rightList">
-            <li><a href="#">dfghjfmd</a></li>
-            <li><a href="#">dfhafgb</a></li>
-            <li><a href="#">khsdds</a></li>
-            <li><a href="#">dhkgds</a></li>
-        </ul>
-      </footer>
-    </div>
-    );
-  }
+export async function Footer() {
+  return <main>ASDASD</main>;
+}
