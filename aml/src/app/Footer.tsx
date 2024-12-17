78d125c7a3746389b0f0d384d267c6868ca0fd30
RossJennison <c0024074@hallam.shu.ac.uk>
2024-12-17 20:43:25 +0000

    footer adjustments

--- aml/src/app/Footer.tsx
+++ aml/src/app/Footer.tsx
@@ -3,24 +3,24 @@ export async function Footer() {
   return (
     <div>
     <footer>
-      <p>&copy; {new Date().getFullYear()} Advanced Media Library</p>
+      <p>  Advanced Media Library {new Date().getFullYear()} &copy;</p>
       <ul id="leftList">
         <li>About</li>
         <li><a href="#">Copyrights</a></li>
         <li><a href="#">Contact us</a></li>
-        <li><a href="#">ghsdfdfsg</a></li>g
+        <li><a href="#">Common FAQs</a></li>
         </ul>    
         <ul>
-          <li><a href="#">dfghjfmd</a></li>
-          <li><a href="#">dfhafgb</a></li>
-          <li><a href="#">khsdds</a></li>
-          <li><a href="#">dhkgds</a></li>
+          <li>Legal</li>
+          <li><a href="#">Privacy notice</a></li>
+          <li><a href="#">Use of Cookies</a></li>
+          <li><a href="#">Accessibility Statement</a></li>
       </ul>
       <ul id="rightList">
-          <li><a href="#">dfghjfmd</a></li>
-          <li><a href="#">dfhafgb</a></li>
-          <li><a href="#">khsdds</a></li>
-          <li><a href="#">dhkgds</a></li>
+          <li>Socials</li>
+          <li><a href="#">Twitter</a></li>
+          <li><a href="#">Facebook</a></li>
+          <li><a href="#">LinkedIn</a></li>
       </ul>
     </footer>
   </div>
